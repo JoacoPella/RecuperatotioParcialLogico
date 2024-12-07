c53ca00d66319a68caf8fde93b74a9d826f1e86e
JoacoPella <jpella@frba.utn.edu.ar>
2024-12-07 10:32:00 -0300

    casi terminado

---
+++ PrologPiedadesPella.pl
@@ -0,0 +1,103 @@
+% Base de Conocimientos
+
+% vive(Persona, Caracteristicas)
+%casa(metrosCuadrador)
+vive(juan, casa(120)).
+vive(fer, casa(110)).
+
+% depto(ambientes, baños)
+vive(nico, depto(3, 2)).
+vive(alf, depto(3, 1)).
+vive(vale, depto(4, 1)).
+
+% loft(añoDeConstruccion)
+vive(julian, loft(2000)).
+
+%Felipe no se modela -> principio de universo cerrado.
+
+%seQuiereMudar(Persona, Lugar).
+seQuiereMudar(rocio, casa(90)).
+
+%vivenEn(Persona, Barrio).
+vivenEn([alf, juan, nico, julian], almagro).
+viveEn(juan, almagro).
+viveEn(nico, almagro).
+viveEn(julian, almagro).
+viveEn(vale, flores).
+viveEn(fer, flores).
+
+%--------------------------------------------------------------
+% Punto 2
+%--------------------------------------------------------------
+
+% esBarrioCopado(Barrio): verdadero si todas las personas del barrio viven en un propiedad copada.
+esBarrioCopado(Barrio):-
+    viveEn(_, Barrio),
+    forall(viveEn(Persona, Barrio), viveEnPropiedadCopada(Persona)).  
+
+viveEnPropiedadCopada(Persona):-
+    vive(Persona, Propiedad),
+    esPropiedadCopada(Propiedad).
+
+esPropiedadCopada(casa(MetrosCuadrados)):-
+    metrosCuadrados > 100.
+
+esPropiedadCopada(depto(CantAmbientes, _)):-
+    CantAmbientes > 3.
+
+esPropiedadCopada(depto(_, CantBanios)):-
+    CantBanios > 1.
+
+esPropiedadCopada(loft(AnioDeConstruccion)):-
+    añoDeConstruccion > 2015.
+
+%--------------------------------------------------------------
+% Punto 3
+%--------------------------------------------------------------
+
+esBarrioCaro(Barrio):-
+    viveEn(_, Barrio),
+    forall(viveEn(Persona, Barrio), not(viveEnPropiedadBarata(Persona))).
+
+viveEnPropiedadBarata(Persona):-
+    vive(Persona, Propiedad),
+    esPropiedadBarata(Propiedad).
+
+esPropiedadBarata(loft(AnioDeConstruccion)):-
+    AnioDeConstruccion < 2005.
+
+esPropiedadBarata(casa(MetrosCuadrados)):-
+    MetrosCuadrados < 90.
+
+esPropiedadBarata(depto(CantAmbientes, _)):-
+    CantAmbientes < 3.
+
+%--------------------------------------------------------------
+% Punto 4
+%--------------------------------------------------------------
+
+% valorDeInmuebleDe(Persona, Valor):  valor expresado en dolares.
+valorDeInmuebleDe(juan, 150000).
+valorDeInmuebleDe(nico, 80000).
+valorDeInmuebleDe(alf, 75000).
+valorDeInmuebleDe(julian, 140000).
+valorDeInmuebleDe(vale, 95000).
+valorDeInmuebleDe(fer, 60000).
+
+% sin recursividad, para comprar una propiedad.
+puedoComprarLaCasaDe(Persona, DineroDisponible):-
+    vive(Persona, _),
+    valorDeInmuebleDe(Persona, Valor),
+    DineroDisponible >= Valor.
+
+
+/*
+quePuedoComprarCon(Dinero) asume que Dinero es un entero y PropiedadesQuePuedoComprar es una lista
+debe ser recursivo usando el predicado sublista([], [])
+sublista([_|cola], Sublista):-
+    sublista(cola, Sublista).
+*/
+% quePuedoComprarCon(Dinero, PropiedadesQuePuedoComprar):-
+
+
+    
